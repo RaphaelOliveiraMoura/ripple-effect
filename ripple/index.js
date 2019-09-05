575d5316212d07580de537b6718dfefd9fd9ef0b
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-09-05 14:09:58 -0300

    ripple class renamed

--- ripple/index.js
+++ ripple/index.js
@@ -1,16 +1,16 @@
-document.querySelectorAll('.container').forEach($button => {
+document.querySelectorAll('.ripple').forEach($button => {
   $button.addEventListener('click', event => {
     const target = event.target;
     const classes = target.classList;
-    if (classes.contains('ripple')) classes.remove('ripple');
+    if (classes.contains('waving')) classes.remove('waving');
     const { offsetX, offsetY } = event;
     target.style.setProperty('--posX', `calc(${offsetX}px - 40px)`);
     target.style.setProperty('--posY', `calc(${offsetY}px - 40px)`);
-    classes.add('ripple');
+    classes.add('waving');
   });
 
   $button.addEventListener('animationend', event => {
     const classes = event.target.classList;
-    classes.remove('ripple');
+    classes.remove('waving');
   });
 });
